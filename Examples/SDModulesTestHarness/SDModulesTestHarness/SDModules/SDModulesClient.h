15fa911e39db888fd0d24aa1e54b0dffbb0fc6c2
Steven Woolgar <Steven_Woolgar@Woolsoft.com>
2014-05-02 15:08:21 -0700

    Reworking how the default layouts are setup.

--- Examples/SDModulesTestHarness/SDModulesTestHarness/SDModules/SDModulesClient.h
+++ Examples/SDModulesTestHarness/SDModulesTestHarness/SDModules/SDModulesClient.h
@@ -17,8 +17,31 @@ typedef void (^SDMConnectCompletionHandler)(NSError*);
 @interface SDModulesClient : NSObject
 
 @property (nonatomic, strong) NSArray* registeredModules;
+@property (nonatomic, strong) NSArray* moduleLayouts;
+
+/*
+ * Get the default layout to use if we can't find the server or it can't return in time for display requirements.
+ */
+
+- (void)setDefaultModuleLayoutsForStates:(NSSet*)applicationStates andRegisteredModules:(NSSet*)modules;
+
+/*
+ * Setup a session with the modules server.
+ *
+ * The application key should be in the form of a reverse dns-like string.
+ * com.walmart.market.platform e.g. com.walmart.walmart.ipad or com.walmart.sams.iphone
+ */
 
 - (void)connectWithApplicationKey:(NSString*)applicationKey andPrivateKey:(NSString*)privateKey withCompletionHandler:(SDMConnectCompletionHandler)completionHandler;
-- (SDModuleLayouts*)moduleLayoutsForStates:(NSSet*)applicationStates andRegisteredModules:(NSSet*)modules withCompletionHandler:(SDMLayoutsCompletionHandler)completionHandler;
+
+/*
+ * Supply a set of states that you support (you will get a layout for each state) as well
+ * as a set of modules that you support.
+ *
+ * This method will fetch the layouts given the constraints supplied in the completion handler
+ * from the server.
+ */
+
+- (void)moduleLayoutsForStates:(NSSet*)applicationStates andRegisteredModules:(NSSet*)modules withCompletionHandler:(SDMLayoutsCompletionHandler)completionHandler;
 
 @end
--- Examples/SDModulesTestHarness/SDModulesTestHarness/SDModules/SDModulesClient.m
+++ Examples/SDModulesTestHarness/SDModulesTestHarness/SDModules/SDModulesClient.m
@@ -13,6 +13,14 @@
 @implementation SDModulesClient
 
 /*
+ * Setup the default layout to use if we can't find the server or it can't return in time for display requirements.
+ */
+
+- (void)setDefaultModuleLayoutsForStates:(NSSet*)applicationStates andRegisteredModules:(NSSet*)modules
+{
+}
+
+/*
  * Setup a session with the modules server.
  *
  * The application key should be in the form of a reverse dns-like string.
@@ -29,15 +37,14 @@
  * Supply a set of states that you support (you will get a layout for each state) as well
  * as a set of modules that you support.
  *
- * This method will return the default layouts to use until the completion handler
- * returns the layouts from the server.
+ * This method will fetch the layouts given the constraints supplied in the completion handler
+ * from the server.
  */
 
-- (SDModuleLayouts*)moduleLayoutsForStates:(NSSet*)applicationStates
-                      andRegisteredModules:(NSSet*)modules
-                     withCompletionHandler:(SDMLayoutsCompletionHandler)completionHandler
+- (void)moduleLayoutsForStates:(NSSet*)applicationStates
+          andRegisteredModules:(NSSet*)modules
+         withCompletionHandler:(SDMLayoutsCompletionHandler)completionHandler
 {
-    return nil;
 }
 
 @end
